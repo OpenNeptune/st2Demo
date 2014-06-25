91e2557d966a95dd1391f53bc2178a32cfbf0223
OpenNeptune <neptunepos@gmail.com>
2014-06-25 22:50:51 +0800

    crud

--- WebContent/curd/empedit.jsp
+++ WebContent/curd/empedit.jsp
@@ -8,36 +8,12 @@
 </head>
 <body>
 	<s:debug></s:debug>
-	<s:form action="emp-add" method="post">
+	<s:form action="emp-update" method="post">
+	    <s:hidden name="employeeId"></s:hidden>
 		<s:textfield name="employeeName" label="employeeName"></s:textfield>
 		<s:radio name="sex" list="#{'男':'男','女':'女' }" label="sex"></s:radio>
 		<s:textfield name="skill" label="skill"></s:textfield>
 		<s:submit value="join-in"></s:submit>
 	</s:form>
-	<hr>
-	<table border="1">
-		<thead>
-			<tr>
-				<th>employeeId</th>
-				<th>employeeName</th>
-				<th>sex</th>
-				<th>skill</th>
-				<th></th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<s:iterator value="#request.employeeList">
-				<tr>
-				<td>${employeeId }</td>
-				<td>${employeeName }</td>
-				<td>${sex }</td>
-				<td>${skill }</td>
-				<td><a href="emp-edit.action?employeeId=${employeeId }">Edit</a></td>
-				<td><a href="emp-delete.action?employeeId=${employeeId }">Delete</a></td>
-				</tr>
-			</s:iterator>
-		</tbody>
-	</table>
 </body>
 </html>
--- src/st2Demo/CRUD/Action/employeeAction.java
+++ src/st2Demo/CRUD/Action/employeeAction.java
@@ -11,11 +11,13 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.RequestAware;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ModelDriven;
 
 import st2Demo.CRUD.CRM.Employee;
 import st2Demo.CRUD.CRM.employeeDao;
 
+@SuppressWarnings("unused")
 public class employeeAction implements RequestAware,ModelDriven<Employee>{
 	
 	private employeeDao dao = new employeeDao();
@@ -42,6 +44,26 @@ public class employeeAction implements RequestAware,ModelDriven<Employee>{
 	 */
 	@Override
 	public Employee getModel() {
+		/**
+		 * 该方法会在调用action的action方法前回将该函数返回的对象压入到值栈中
+		 * 
+		 * 因此可以通过通过盘的employee对象的id属性来判断当前操作是对现有的对象做修改还是新增
+		 * （需要在该action中添加一个id属性，同时需要注意为了在做判断前为id属性赋值，
+		 *  此处需要使用paramsPrepareParamsStack拦截器栈
+		 *  在package中加入<!-- 修改该包中的默认的拦截器栈 -->
+		 *  <default-interceptor-ref name="paramsPrepareParamsStack"></default-interceptor-ref>
+		 *  ）
+		 *  
+		 * 
+		 */
+		//为action添加employeeId
+//		if(employeeId ==  null){
+//			//新增操作
+//			employee = new Employee();
+//		}else{
+//			//对现有的做修改
+//			employee = dao.getById(employeeId);
+//		}
 		employee = new Employee();
 		return employee;
 	}
@@ -65,6 +87,24 @@ public class employeeAction implements RequestAware,ModelDriven<Employee>{
 		return "success";
 	}
 	
+	public String updateAction(){
+		dao.edit(employee);
+		return "success";
+	}
+	public String editAction(){
+		//下面的方式不能完成表单回显
+		//因为employee对象新建后与valuestack栈中的对象就断绝关系，成俩个对立的对象
+		//employee = dao.getById(employee.getEmployeeId());
+		
+		
+		//1.首先该action实现了ModelDriven接口，所以edit?employeeId=xx提交的时候employee对象的id被赋值了
+		//2.通过dao获取一个employeed对象并将其压入valuestack，中用于回显
+		//3.注意此时valuestack中有俩个valuestack对象
+		//ActionContext.getContext().getValueStack().push(dao.getById(employee.getEmployeeId()));
+		
+		
+		return "emp-edit";
+	}
 
 
 }
